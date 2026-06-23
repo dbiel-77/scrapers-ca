8027a2377b4f861351aeb6ba4f77ae111934db93
Daniel Biel <daniel.biel@glocalfoundation.ca>
2026-06-23 10:52:13 -0400

    fix: delta organization patch in init

--- ca_bc_delta/__init__.py
+++ ca_bc_delta/__init__.py
@@ -1,3 +1,5 @@
+from pupa.scrape import Organization
+
 from utils import CanadianJurisdiction
 
 
@@ -7,3 +9,16 @@ class Delta(CanadianJurisdiction):
     division_name = "Delta"
     name = "Delta City Council"
     url = "https://www.delta.ca"
+
+    def get_organizations(self):
+        organization = Organization(self.name, classification=self.classification)
+
+        organization.add_post(role="Mayor", label=self.division_name, division_id=self.division_id)
+        for seat_number in range(1, 7):
+            organization.add_post(
+                role="Councillor",
+                label=f"{self.division_name} (seat {seat_number})",
+                division_id=self.division_id,
+            )
+
+        yield organization
